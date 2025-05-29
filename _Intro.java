af5420ecbe7d9571f2b3961312aab935bb805a64
IMOitself <russjonn@gmail.com>
2025-05-29 16:53:47 +0800

    move test screen to player screen

--- _Intro.java
+++ _Intro.java
@@ -34,13 +34,10 @@ public class _Intro {
 		UI.colorGrey();
 		UI.printBox(Player.getTopPlayers());
 		UI.colorReset();
-        UI.printBox("0. test hehe\n1. Login\n2. Register\n3. Exit");
+        UI.printBox("1. Login\n2. Register\n3. Exit");
         input = scanner.nextLine();
 
         switch (input) {
-            case "0":
-                testScreen(scanner);
-                break;
             case "1":
                 loginScreen(scanner);
                 break;
@@ -115,38 +112,6 @@ public class _Intro {
                 scanner.nextLine();
                 mainMenu(scanner);
             }
-        }
-    }
-
-    public static void testScreen(Scanner scanner) {
-        String input = "";
-
-        UI.clearScreen();
-        UI.printBox("1. Phase 1\n2. Phase 2\n3. Phase 3\n4. Phase 4\n5. Final\n6. Back");
-        input = scanner.nextLine();
-
-        switch (input) {
-            case "1":
-                _Phase1.exampleScreen(scanner);
-                break;
-            case "2":
-                _Phase2.exampleScreen(scanner);
-                break;
-            case "3":
-                _Phase3.exampleScreen(scanner);
-                break;
-            case "4":
-                _Phase4.exampleScreen(scanner);
-                break;
-            case "5":
-                _PhaseFinal.exampleScreen(scanner);
-                break;
-            case "6":
-                mainMenu(scanner);
-                break;
-            default:
-                testScreen(scanner);
-                break;
         }
     }
 
--- _PlayerScreen.java
+++ _PlayerScreen.java
@@ -3,8 +3,47 @@ import java.util.Scanner;
 public class _PlayerScreen {
     public static void exampleScreen(Scanner scanner){
         UI.clearScreen();
+        UI.printGreyText("[ Work in progress ]");
         UI.printBox("Username: " + Player.username + "\nPassword: " + Player.password + "\nCurrent Phase: " + Player.currentPhase + "\nCurrent Score: " + Player.currentScore);
-        scanner.nextLine();
-        _Intro.mainMenu(scanner);
+        UI.printGreyText("\nPress Enter To go to Main Menu...\n0. Test hehe");
+        String input = scanner.nextLine();
+        if(input.equals("0")){
+            testScreen(scanner);
+        }
+        else{
+            _Intro.mainMenu(scanner);
+        }
+    }
+
+    public static void testScreen(Scanner scanner) {
+        String input = "";
+
+        UI.clearScreen();
+        UI.printBox("1. Phase 1\n2. Phase 2\n3. Phase 3\n4. Phase 4\n5. Final\n6. Back");
+        input = scanner.nextLine();
+
+        switch (input) {
+            case "1":
+                _Phase1.exampleScreen(scanner);
+                break;
+            case "2":
+                _Phase2.exampleScreen(scanner);
+                break;
+            case "3":
+                _Phase3.exampleScreen(scanner);
+                break;
+            case "4":
+                _Phase4.exampleScreen(scanner);
+                break;
+            case "5":
+                _PhaseFinal.exampleScreen(scanner);
+                break;
+            case "6":
+                _Intro.mainMenu(scanner);
+                break;
+            default:
+                testScreen(scanner);
+                break;
+        }
     }
 }
