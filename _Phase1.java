fa3c96998152e2aab8245d71d246a8f54d43bfb4
Itooonn <karlmahusay2@gmail.com>
2025-06-06 18:23:23 +0800

    test

--- _Phase1.java
+++ _Phase1.java
@@ -272,11 +272,11 @@ public class _Phase1 {
 		        
 		        resetTimer(); // <------- reset timer for every correct input									        	
 		    } 
-			if (input1.isEmpty()){
-				UI.clearScreen();
+			// if (input1.isEmpty()){
+			// 	UI.clearScreen();
 
-				System.out.println("Test");
-			}
+			// 	System.out.println("Test");
+			// }
 			else { //<------ IF THE INPUT IS WRONG
 				UI.clearScreen();
 		        UI.printBox("\nWRONG");
