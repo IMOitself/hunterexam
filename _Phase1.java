d3b640dd99c1d071a0ef8b8bf7c1d51d120af707
Itooonn <karlmahusay2@gmail.com>
2025-06-06 14:55:52 +0800

    bug fixes

--- _Phase1.java
+++ _Phase1.java
@@ -273,12 +273,7 @@ public class _Phase1 {
 		        resetTimer(); // <------- reset timer for every correct input
 				
 					        	
-		    } if (input1.isEmpty()){
-				UI.clearScreen();
-				System.out.println("\nWord to match: " + word); // Print word before asking for input
-				System.out.print("Input here: ");
-				scanner.nextLine();
-			}
+		    } 
 			else { //<------ IF THE INPUT IS WRONG
 		        UI.printBox("WRONG");
 		        wrongInput ++;
@@ -291,6 +286,15 @@ public class _Phase1 {
 		    }
 
 		    currentWordIndex++;
+			
+			while (input1.isEmpty()){
+				UI.clearScreen();
+				System.out.println("You have "+ TIME_LIMIT + " seconds\n");
+
+				System.out.println("\nWord to match: " + word); // Print word before asking for input
+				System.out.print("Input here: ");
+				scanner.nextLine();
+			}
 	    }
 		if (wrongInput == 10) {
 			failedTest();
